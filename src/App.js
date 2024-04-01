5068476c78323c4d616e578b068fa5f861f53878
ehsan304 <muhammadehsan304@gmail.com>
2024-04-01 17:53:32 +0500

    complete

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Feed from'./components/Feed';
 import VideoDetail from'./components/VideoDetail';
 import ChannelDetail from'./components/ChannelDetail';
 import SearchFeed from'./components/SearchFeed';
-
-
+import SearchBar from './components/SearchBar';
+import Sidebar from './components/Sidebar';
+import Videos from './components/Videos';
+import VideoCard from './components/VideoCard';
+import ChannelCard from './components/ChannelCard';
+import LoaderFile from './components/LoaderFile';
 const App = () => (
     <BrowserRouter>
-        <Box src={{ background:' #0000'}}>
+        <Box sx={{ backgroundColor:' #000'}}>
             <Navbar/>
             <Routes>
                 <Route path='/' exact element={<Feed />} />
